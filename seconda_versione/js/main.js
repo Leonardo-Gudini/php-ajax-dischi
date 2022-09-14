b85bfe44a9b6981bb0e60a2675a823487ebda75c
Leonardo Gudini <leonardo.gudini98@gmail.com>
2022-09-14 18:20:58 +0200

    add: json_encode, axios call and v-for

---
+++ seconda_versione/js/main.js
@@ -0,0 +1,18 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+      discList: [],
+    },
+    created() {
+
+
+        for (let i=0; i<=5; i++){
+            axios.get("http://localhost:8888/php-ajax-dischi/seconda_versione/database.php")
+            .then(disc => {
+                this.discList.push(disc.data[i])
+                console.log(disc.data[i])
+            })
+        }
+    }
+
+})
